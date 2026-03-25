6c26d04abd9a85ff841c89f2ef831b6da6734ff6
veradec <glitchmill.dyj3d@simplelogin.co>
2026-03-25 19:38:20 +0530

    readme: added readme and checklist

---
+++ README.md
@@ -0,0 +1,12 @@
+What is the Language Okabe?
+
+Okabe is a language that is nothing at this point, but I want to make it a language that is 
+
+[] Turing Complete
+[] Selfhosted
+
+// can come at a later point
+[] Has Multiple Packages that I can use for my project(s)
+
+
+but these are essentially my check list for this language, to check for the syntax. check out main.ok in the tree, thx :3
--- okabe.c
+++ okabe.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+// Return codes are used to find comments and empty lines
+// rc : meaning
+// 1  : Line starts with  //
+// 2  : Line starts with /* (start of a multiline comment)
+//-2  : Line starts with */ (end of a multine)
+// 3  : Line is just a empty \n
+
 int find_if_comment(char* line){
   int linesize = sizeof(line)/sizeof(line[0]);
+  if(line[0] == '\n')    { return 3; }
+
   for (int i = 0; i < linesize; ++i) {
     if('/' == line[i]){
       if(line[i+1] == '/'){
@@ -18,6 +27,12 @@ int find_if_comment(char* line){
   return 0;
 }
 
+/* Find Typedefs and Substiture macros all over the file */
+int find_if_macro(char* line){
+  
+}
+
+
 int main()
 {
   FILE *fp = fopen("main.ok", "r");
@@ -27,12 +42,14 @@ int main()
   }
   char buf[99];
   bool setMultiLine = false; 
+
   while(fgets(buf, 99, fp)){
+
+
     int find_comm = find_if_comment(buf);
     if(find_comm == 1){
       continue;
     }
-
     if(find_comm == 2){
       setMultiLine = true;
       continue;
@@ -40,7 +57,15 @@ int main()
 	setMultiLine = false;
 	continue;
     }
+    if(find_comm == 3){
+      continue;
+    }
 
+    if(find_comm == 4){
+      printf("%d", find_comm);
+    }
+
+    
     if(setMultiLine){
       continue;
     }
